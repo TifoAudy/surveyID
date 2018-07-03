22e92c6542b3ea66006f608d5b6bfcb01a787a75
Tifo Audi Alif Putra <tifoaudy@Tifos-MacBook-Pro.local>
2018-07-03 14:48:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//initial express app
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+});
+
+//process.env.PORT = used for production
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
