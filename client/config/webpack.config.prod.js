f5b85496dd15852d40ad9e48467ec72ef6a1f3b3
Tifo Audi Alif Putra <tifoaudy@Tifos-MacBook-Pro.local>
2018-07-16 14:03:33 +0700

    update webpack prod

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -166,46 +166,24 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /(\.css|\.scss)$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
-                  fallback: {
-                    loader: require.resolve('style-loader'),
-                    options: {
-                      hmr: false,
-                    },
-                  },
+                  fallback: 'style-loader',
                   use: [
                     {
-                      loader: require.resolve('css-loader'),
+                      loader: 'css-loader',
                       options: {
+                        modules: true,
+                        sourceMap: true,
                         importLoaders: 1,
-                        minimize: true,
-                        sourceMap: shouldUseSourceMap,
-                      },
+                        localIdentName: '[name]--[local]--[hash:base64:8]'
+                      }
                     },
-                    {
-                      loader: require.resolve('postcss-loader'),
-                      options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: 'postcss',
-                        plugins: () => [
-                          require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
-                        ],
-                      },
-                    },
-                  ],
+                    'postcss-loader',
+                    'sass-loader'
+                  ]
                 },
                 extractTextPluginOptions
               )
